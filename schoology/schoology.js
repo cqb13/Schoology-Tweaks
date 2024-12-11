c30f64aa58dbc4a62476185cadeb2abff524867e
cqb13 <cqb13.main@gmail.com>
2024-12-11 16:16:45 -0500

    more tweaks

--- schoology/schoology.js
+++ schoology/schoology.js
@@ -5,3 +5,37 @@ const customLogo = document.querySelector(
 if (customLogo) {
   customLogo.remove();
 }
+
+const allDivs = document.querySelectorAll("button[aria-label]");
+
+let foundSearchIcon = false;
+let foundMessageCount = false;
+let foundNotificationCount = false;
+
+for (const div of allDivs) {
+  const ariaLabel = div.getAttribute("aria-label");
+  if (ariaLabel == "Search") {
+    div.remove();
+    foundSearchIcon = true;
+  }
+
+  if (/^\d+ unread messages$/.test(ariaLabel)) {
+    const spanChild = div.querySelector("span");
+    if (spanChild) {
+      spanChild.remove();
+      foundMessageCount = true;
+    }
+  }
+
+  if (/^\d+ unread notification$/.test(ariaLabel)) {
+    const spanChild = div.querySelector("span");
+    if (spanChild) {
+      spanChild.remove();
+      foundNotificationCount = true;
+    }
+  }
+
+  if (foundSearchIcon && foundMessageCount && foundNotificationCount) {
+    break;
+  }
+}
